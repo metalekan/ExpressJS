cfc7e27ae386c6f7281f3790709e45d355cc60e7
Meta Lekan <metadelekan@gmail.com>
2023-08-29 23:36:36 +0100

    This is first commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const http = require('http')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('./public'))
+
+// app.get('/', (req, res)=> {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
+
+app.all('/', (req, res)=> {
+  res.status(404).send('Resource not found')
+})
+
+app.listen(5050, ()=> {
+  console.log('server is listenning on port 5050..........')
+})
--- final/03-express-basics.js
+++ final/03-express-basics.js
@@ -14,7 +14,7 @@ app.all('*', (req, res) => {
   res.status(404).send('<h1>resource not found</h1>')
 })
 
-app.listen(5000, () => {
+app.listen(5050, () => {
   console.log('server is listening on port 5000...')
 })
 
